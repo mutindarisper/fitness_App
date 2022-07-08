24e3132144d0f6e576d84ec00f930482eef82187
unknown <mutindarisper@gmail.com>
2022-07-08 17:21:15 +0300

    implemented markers on map

--- src/pages/MyActivity.js
+++ src/pages/MyActivity.js
@@ -1,86 +1,121 @@
 import React, {useRef, useEffect, useState} from 'react'
+import ReactDOM from "react-dom";
 // import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
+// import Map, { GeolocateControl } from "react-map-gl";
 
 
 import { Box, Stack, Typography } from '@mui/material'
+mapboxgl.accessToken =
+  "pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA";
 
+
+
+  const Marker = ({ onClick, children, feature }) => {
+    const _onClick = () => {
+      onClick(feature.properties.description);
+    };
+  
+    return (
+      <button onClick={_onClick} className="marker">
+        {children}
+      </button>
+    );
+  };
 
 
 
 const MyActivity = () => {
+   
 
 
-    const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(36.8);
-const [lat, setLat] = useState(-1.3);
-const [zoom, setZoom] = useState(14);
+    const mapContainerRef = useRef(null);
 
-
-useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-    container: mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v11',
-    accessToken: 'pk.eyJ1IjoiY2hyaXNiYXJ0IiwiYSI6ImNrZTFtb3Z2bDAweTMyem1zcmthMGY0ejQifQ.3PzoCgSiG-1-sV1qJvO9Og',
-    center: [lng, lat],
-    zoom: zoom
-    });
-    });
-
+    // Initialize map when component mounts
     useEffect(() => {
-        if (!map.current) return; // wait for map to initialize
-        map.current.on('move', () => {
-        setLng(map.current.getCenter().lng.toFixed(4));
-        setLat(map.current.getCenter().lat.toFixed(4));
-        setZoom(map.current.getZoom().toFixed(2));
-        });
+        const map = new mapboxgl.Map({
+          container: mapContainerRef.current,
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: [36.8, -1.3],
+          zoom: 10,
         });
 
-        console.log( mapContainer.current, 'mapcontainer')
 
+
+
+        const geoJson = {
+                        type: 'FeatureCollection',
+                        features: [
+                          {
+                            type: 'Feature',
+                            geometry: {
+                              type: 'Point',
+                              coordinates: [36.8, -1.3]
+                            },
+                            properties: {
+                              title: 'Mapbox',
+                              description: 'Washington, D.C.'
+                            }
+                          },
+                          {
+                            type: 'Feature',
+                            geometry: {
+                              type: 'Point',
+                              coordinates: [36.85, -1.32]
+                            },
+                            properties: {
+                              title: 'Mapbox',
+                              description: 'San Francisco, California'
+                            }
+                          }
+                        ]
+                      };
+    
+        // // Render custom marker components
+        geoJson.features.forEach((feature) => {
+          // Create a React ref
+          const ref = React.createRef();
+          // Create a new DOM node and save it to the React ref
+          ref.current = document.createElement("div");
+          // Render a Marker Component on our new DOM node
+          ReactDOM.render(
+            <Marker onClick={markerClicked} feature={feature} />,
+            ref.current
+          );
+    
+          // Create a Mapbox Marker at our new DOM node
+          new mapboxgl.Marker(ref.current)
+            .setLngLat(feature.geometry.coordinates)
+            .addTo(map);
+        });
+    
+        // Add navigation control (the +/- zoom buttons)
+        map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    
+        // Clean up on unmount
+        return () => map.remove();
+      }, []);
         // const origin = [36.8, -1.3];
 
-        const geojson = {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [36.8, -1.3]
-                },
-                properties: {
-                  title: 'Mapbox',
-                  description: 'Washington, D.C.'
-                }
-              },
-              {
-                type: 'Feature',
-                geometry: {
-                  type: 'Point',
-                  coordinates: [36.95, -1.39]
-                },
-                properties: {
-                  title: 'Mapbox',
-                  description: 'San Francisco, California'
-                }
-              }
-            ]
-          };
+//        
 
 
-          // add markers to map
-for (const feature of geojson.features) {
-    // create a HTML element for each feature
-    const el = document.createElement('div');
-    el.className = 'marker';
-    console.log( map.current, 'map current ')
-    // make a marker for each feature and add to the map
-    new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map.current);
-  }
+//           // add markers to map
+// for (const feature of geojson.features) {
+//     // create a HTML element for each feature
+//     const el = document.createElement('div');
+//     el.className = 'marker';
+//     console.log( map.current, 'map current ')
+//     // make a marker for each feature and add to the map
+//     // new mapboxgl.Marker(el).setLngLat(feature.geometry.coordinates).addTo(map.current);
+//   }
+
+const markerClicked = (title) => {
+    window.alert(title);
+  };
+
+  
 
 
   return (
@@ -89,11 +124,22 @@ for (const feature of geojson.features) {
     <Typography variant='h3' mb='46px'>
         My Activity
       </Typography>
-      <div className="sidebar">
+     
+    
+   
+
+    <div div className="map-container" ref={mapContainerRef} >
+    {/* <div className="sidebar">
     Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+    </div> */}
+    <button className='geolocation'>My Location</button>
+
     </div>
 
-<div ref={mapContainer} className="map-container" />
+
+
+
+
 
 
     </div>
